fea522bb7fa1f1b407333ef6e1dfe57e858dc873
Owner <braydens.coding@gmail.com>
2024-10-28 21:08:30 -0500

    Task 1 & 2 [COMPLETED]

--- queries.sql
+++ queries.sql
@@ -1,7 +1,17 @@
 --Part 1
-
+SELECT
+    COLUMN_NAME, DATA_TYPE
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_NAME = 'job';
 --Part 2
-
+SELECT
+    name
+FROM
+    employer
+WHERE
+    location = "St. Louis City";
 --Part 3
 
 --Part 4
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank
+    @Size(min = 3, max = 60, message = "Location must be between 3 and 60 characters.")
+    private String location;
+
+    public @NotBlank @Size(min = 3, max = 60, message = "Location must be between 3 and 60 characters.") String getLocation() {
+        return location;
+    }
+
+    public void setLocation(@NotBlank @Size(min = 3, max = 60, message = "Location must be between 3 and 60 characters.") String location) {
+        this.location = location;
+    }
+
+    public Employer() {}
 }
